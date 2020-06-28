74deb0370a37419f9e992aed6df3717f3bcdc000
Filip Šedivý <mail@filipsedivy.cz>
2020-06-28 02:39:27 +0200

    Fix PHP Code Sniffer

--- tests/cases/PasswordVerifyTest.php
+++ tests/cases/PasswordVerifyTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Cases;
 
 use ExtKit\PasswordVerify\Combination\NotVerifyInitialLetterCombination;
+use ExtKit\PasswordVerify\Exceptions\RuntimeException;
 use ExtKit\PasswordVerify\Exceptions\ShortTextException;
 use ExtKit\PasswordVerify\PasswordVerify;
 use Tester\Assert;
 use Tester\TestCase;
 use Throwable;
+use TypeError;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -30,6 +32,46 @@ final class PasswordVerifyTest extends TestCase
         $values = ['Test.', 'test.', 'Test'];
 
         Assert::equal($values, $verify->getAllowableValues());
+    }
+
+    public function testAddCombination(): ?Throwable
+    {
+        $verify = new PasswordVerify('.');
+
+        return Assert::exception(static function () use ($verify): void {
+            $verify->addCombination(new NotVerifyInitialLetterCombination());
+        }, RuntimeException::class);
+    }
+
+    public function testVerify(): ?Throwable
+    {
+        $verify = new PasswordVerify('test.');
+        $verify->setDefaultCombinations();
+
+        $result = $verify->verify(static function (string $value): bool {
+            return md5($value) === md5('test');
+        });
+
+        Assert::true($result);
+
+        $result = $verify->verify(static function (string $value): bool {
+            return md5($value) === '...';
+        });
+
+        Assert::false($result);
+
+        $verify->deleteAllCombinations();
+
+        $result = $verify->verify(static function (string $value): bool {
+            return md5($value) === md5('test');
+        });
+
+        Assert::false($result);
+
+        return Assert::exception(static function () use ($verify): void {
+            $verify->verify(static function (): void {
+            });
+        }, TypeError::class);
     }
 }
 
